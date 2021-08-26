b1f7cada1f4224bbed970fc8f6930f248b7ae7cb
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-08-26 13:59:51 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("and that piece of art is useful" - Dora Korpar, 2015-10-19")
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
