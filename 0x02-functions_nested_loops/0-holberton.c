5f76d905a06e4f3320bfa9e12cdd3e4e3ed0acb2
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-08-31 22:14:34 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,13 +2,14 @@
 int main(void)
 {
 
-_putchar("_");
-_putchar("P");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
+}
