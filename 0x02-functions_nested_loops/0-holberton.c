4e16062f998a5b54110559ea8587a47c427f241f
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-08-31 22:00:48 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,14 @@
 #include "main.h"
 int main(void)
 {
-char *c = "_putchar";
-int i;
- for(i = 0; i <sizeof (c); i++){
-_putchar(c[i]);
-}
- _putchar('\n');
+
+_putchar("_");
+_putchar("P");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar('\n');
 return (0);
