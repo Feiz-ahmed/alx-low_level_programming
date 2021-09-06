988eb1f85abdd46753077cba0b25455e29df0463
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-06 08:40:03 -0700

    hn

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - calculate the length of a string
+ * @s: the string to calculate the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+char *str = "holberton";
+putchar("%d", strlen(str));
+
+return (o);
