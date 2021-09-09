1207ec96fb509601210f691de87fef1d685143fe
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-09 12:58:59 -0700

    mandatory task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
- *_strcmp - used to compare two strings in an array
+ *_strcmp - compare two strings
  *@s1:string to be declared
  *@s2:string to be compared
- *Return ( 0 if (s1 = s2)
+ *Description: This functions compares two strings pointed to by s1 and s2.
+ * Return: 0 if s1 matches s2,
+ *otherwise an integer less than 0 if s1 is less than s2,
+ *otherwise an integer greater than 0 if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
