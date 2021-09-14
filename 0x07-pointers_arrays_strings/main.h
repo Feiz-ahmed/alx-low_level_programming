775abd304efe260340c143f61473620d146815c6
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-14 09:33:56 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
