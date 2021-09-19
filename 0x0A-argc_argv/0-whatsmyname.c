791036523144fb8a4453b6b375d704e60fd11ba5
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-19 06:16:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main- includes the function
+ *@argc: size of the argument vector
+ * @argv: program name and arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}
