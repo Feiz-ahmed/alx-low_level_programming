214b744595ab09e4a02b7c1a57901698d1a09ba6
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-22 15:34:24 -0700

    1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-size_t strlen(const char *str);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
