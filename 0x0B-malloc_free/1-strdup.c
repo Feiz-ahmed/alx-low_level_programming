400c12cd817eb6934e6947e4a6dc433462d231af
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-22 00:39:09 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *strup- function that returns duplicates of another str
+ *@str : original str
+ *Return :return pointer to the duplicated str 
+  */
+char *_strdup(char *str);
+int str size;
+char *new_string
+strdupe = (char *)malloc(sizeofchar)*
