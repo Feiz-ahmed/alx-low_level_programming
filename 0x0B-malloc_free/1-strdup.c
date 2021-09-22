f621cfbb764e1f90c224c9b3be4150a2aad5e9a6
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-22 03:28:00 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,26 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
- *strup- function that returns duplicates of another str
- *@str : original str
- *Return :return pointer to the duplicated str 
+ * _strdup - function that returns duplicates of another str
+ * @str: original str
+ * Return:return pointer to the duplicated str
   */
-char *_strdup(char *str);
-int str size;
-char *new_string
-strdupe = (char *)malloc(sizeofchar)*
+char *_strdup(char *str)
+{
+int i = 0;
+char *new_string;
+int str_len = 0;
+while (str[str_len] != 0)
+str_len++;
+new_string = malloc(sizeof(char) * str_len + 1);
+if (new_string == NULL)
+return (NULL);
+while (str)
+{
+new_string[i] = *str;
+i++;
+str++;
+}
+return (new_string);
+}
