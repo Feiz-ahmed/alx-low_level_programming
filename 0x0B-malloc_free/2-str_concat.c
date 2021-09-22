b159ce6d1433ab51a528c61396ad2d27ad830c72
Feiz-ahmed <Feizaahmed2000@gmail.com>
2021-09-22 15:58:22 -0700

    er

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,30 +1,51 @@
 #include "main.h"
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- *str_concat-concates two strings
- *@s1: string 1
- *@s2: string 2 
- *Return : a pointer to the function
+ * str_concat - concatenate two strings
+ * @s1: a pointer to the first string
+ * @s2: a pointer to the second string
+ *
+ * Return: NULL if memory allocation fails,
+ * otherwise a pointer to a the new string
  */
 char *str_concat(char *s1, char *s2)
 {
-  int cnt = _strlen(s1) + _strlen(s2);
-  int i = 0;
-  char *str = malloc(cnt + 1);
-  if (!s1 || !s2 || !str)
-    return (0);
-      for (; i < cnt; i++);
-  {
-    if (i < _strlen(s1))
-      str[i] = s1[i];
-    else if (i <= _strlen(s1) && *s2 != 0)
-      {
-	str[i] = *s2;
-	s2++;
-      }
-  }
-  str[i] = 0;
-  return (str);
+unsigned int len1 = 0;
+unsigned int len2 = 0;
+char *cat;
+
+if (s1)
+{
+while (s1[len1])
+++len1;
+}
+else
+{
+s1 = "";
+}
+if (s2)
+{
+while (s2[len2])
+++len2;
+}
+else
+{
+s2 = "";
+}
+cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+if (!cat)
+return (NULL);
+if (s1)
+{
+for (len1 = 0; s1[len1]; ++len1)
+cat[len1] = s1[len1];
+}
+if (s2)
+{
+for (len2 = 0; s2[len2]; ++len2)
+cat[len1 + len2] = s2[len2];
+}
+cat[len1 + len2] = '\0';
+return (cat);
 }
